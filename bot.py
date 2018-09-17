00a1b26846bf3b1a526aa93afd6e4c63f0880fca
Joy Israngkul [Worawit Israngkul] <worawit.isr@mahidol.edu>
2018-09-18 05:03:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,23 +12,23 @@ handler = WebhookHandler('28eb0ef0b53631f1581716b309b1acbb')
 def hello():
     return "Hello AJoy Linebot v2 World!"
 
-@app.route("/webhook", methods=['POST','GET'])
+@app.route("/webhook", methods=['GET','POST'])
 def webhook():
     if request.method == 'POST':
         
     return 'OK'
-    # get X-Line-Signature header value
-    #signature = request.headers['X-Line-Signature']
+# get X-Line-Signature header value
+#signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    #body = request.get_data(as_text=True)
-    #app.logger.info("Request body: " + body)
+# get request body as text
+#body = request.get_data(as_text=True)
+#app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    #try:
-    #    handler.handle(body, signature)
-    #except InvalidSignatureError:
-    #    abort(400)
+# handle webhook body
+#try:
+#    handler.handle(body, signature)
+#except InvalidSignatureError:
+#    abort(400)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
