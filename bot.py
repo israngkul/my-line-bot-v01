46a766bea762adeabac07a712b0da224c831f911
Joy Israngkul [Worawit Israngkul] <worawit.isr@mahidol.edu>
2018-09-18 20:40:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,11 +28,13 @@ def webhook():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    #profile = line_bot_api.get_profile(user_id)
+    result = json.loads(body)
+    user_id = data['events'][0]['source']['userId']
     linenotify(str(body))
-    #linenotify(profile.user_id)
-    #linenotify(profile.picture_url)
-    #linenotify(profile.status_message)
+    profile = line_bot_api.get_profile(user_id)
+    linenotify(profile.user_id)
+    linenotify(profile.picture_url)
+    linenotify(profile.status_message)
     # handle webhook body
     try:
         handler.handle(body, signature)
