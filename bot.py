14cf7f9dd265df2e7200cb04e50b18450b4e89c6
Joy Israngkul [Worawit Israngkul] <worawit.isr@mahidol.edu>
2018-09-26 16:36:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,11 +68,11 @@ def handle_message(event):
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ข้อความจาก :'+event.message.text))
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='ข้อความจาก :'+profile.display_name))
     #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=profile.display_name+' \nภาพโปร์ไฟล์ของผู้ส่งข้อความ:'+profile.picture_url))
-    text = event.message.txt
+    text = str(event.message.txt)
     words = text.spit()
-    linenotify(words[0])
-    linenotify(words[1])
-    linenotify(words[2])
+    for word in words
+        linenotify(word)
+
     
     
 
