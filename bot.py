99d106a46c96667f7b82d7df13fb7cd70e04dbc3
Joy Israngkul [Worawit Israngkul] <worawit.isr@mahidol.edu>
2018-09-27 14:22:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ def exchange(currency):
     resulttext = "ที่มาของข้อมูลตอบกลับ ณ เวลา :"+str(rate['result']['timestamp'])+"\n"
     resulttext = resulttext+ str(rate['result']['data']['data_header']['report_name_th'])+"\n"
     resulttext = resulttext+ str(rate['result']['data']['data_header']['report_uoq_name_th'])+"\n"
-    resulttext = resulttext+ "แหล่งข้อมูลจาก : "str(rate['result']['data']['data_header']['report_source_of_data'][0]['source_of_data_th'])+"\n"
+    resulttext = resulttext+ "แหล่งข้อมูลจาก : "+str(rate['result']['data']['data_header']['report_source_of_data'][0]['source_of_data_th'])+"\n"
     resulttext = resulttext+ "สกุลเงิน - "+str(rate['result']['data']['data_detail'][0]['currency_name_eng'])+" "+str(rate['result']['data']['data_detail'][0]['currency_name_th'])+"\n"
     # อัตราขายถั่วเฉลี่ย
     resulttext = resulttext+ "อัตราขายถั่วเฉลี่ย : "+str(rate['result']['data']['data_detail'][0]['selling'])+"\n"
